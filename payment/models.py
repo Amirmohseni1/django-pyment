7886d6c4b642dc064cecf4303f912c9d75b3cef7
Amir Mohseni <amirmohseni4575@gmail.com>
2023-11-27 23:28:54 +0330

    feat: base models and add requirements file

--- payment/models.py
+++ payment/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Payment(models.Model):
+    class Meta:
+        app_label = _("payment")
+        verbose_name = _("payment")
+        verbose_name_plural = _("payments")
