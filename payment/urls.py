16a1a6b0045ab142a036f1c069905217c394df65
Amir Mohseni <amirmohseni4575@outlook.com>
2023-11-28 13:36:36 +0330

    feat: add AbstractPayment ,Payment model and config admin

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from .views import VerifyView
 
 app_name = "payment"
-urlpatterns = [
-    path('verify/', VerifyView, name='Verify')
-]
+urlpatterns = [path("verify/", VerifyView, name="Verify")]
